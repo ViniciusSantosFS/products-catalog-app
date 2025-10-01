916e1fa4f13a8309e758e0ee24143541f76a0601
Vinicius Santos <viniciusantos.florencio@gmail.com>
2025-10-01 07:55:29 -0300

    chore: setup bottom sheet

--- src/main/App.tsx
+++ src/main/App.tsx
@@ -1,10 +1,11 @@
+import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import { QueryClientProvider } from '@tanstack/react-query';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { HttpGetClient } from '../data/http';
 import { RemoteCategoryList, RemoteProductList } from '../data/usecases';
 import { AxiosHttpClient } from '../infra';
 import { HomeScreen } from '../presentation/screens';
 import { queryClient } from '../presentation/shared/react-query';
-
 const makeAxiosHttpClient = (): HttpGetClient => {
   return new AxiosHttpClient();
 };
@@ -14,14 +15,21 @@ export default function App() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <HomeScreen
-        productListUseCase={
-          new RemoteProductList(apiUrl, makeAxiosHttpClient())
-        }
-        categoryListUseCase={
-          new RemoteCategoryList(`${apiUrl}/categories`, makeAxiosHttpClient())
-        }
-      />
+      <GestureHandlerRootView>
+        <BottomSheetModalProvider>
+          <HomeScreen
+            productListUseCase={
+              new RemoteProductList(apiUrl, makeAxiosHttpClient())
+            }
+            categoryListUseCase={
+              new RemoteCategoryList(
+                `${apiUrl}/categories`,
+                makeAxiosHttpClient(),
+              )
+            }
+          />
+        </BottomSheetModalProvider>
+      </GestureHandlerRootView>
     </QueryClientProvider>
   );
 }
